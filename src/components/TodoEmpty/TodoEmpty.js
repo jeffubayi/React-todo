e1345cd8395c1027d4159db13909ae617a900533
jeffubayi <jeffubayi@gmail.com>
2020-09-16 00:31:59 +0300

    TodoEmpty - create component

--- src/components/TodoEmpty/TodoEmpty.js
+++ src/components/TodoEmpty/TodoEmpty.js
@@ -5,3 +5,21 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItemText from "@material-ui/core/ListItemText";
 import Avatar from "@material-ui/core/Avatar";
 import Icon from "@material-ui/core/Icon";
+
+const ToDoEmpty = ({ message }) => (
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar>
+          <Icon>done_all</Icon>
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={message} />
+    </ListItem>
+  );
+  
+  ToDoEmpty.propTypes = {
+    message: PropTypes.string.isRequired,
+  };
+  
+  export default ToDoEmpty;
+  
