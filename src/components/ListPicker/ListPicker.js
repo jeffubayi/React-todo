e6f213870b2b2ad1c18e05e2f8dc3dd945767c7e
jeffubayi <jeffubayi@gmail.com>
2020-09-24 09:43:45 +0300

    ListPicker - create a ListPicker component to display lists of todo items

--- src/components/ListPicker/ListPicker.js
+++ src/components/ListPicker/ListPicker.js
@@ -9,3 +9,15 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import { Paper, Typography, Button } from "@material-ui/core";
+
+class ListPicker extends React.Component {
+    myInput = React.createRef();
+  
+    goToList = (event) => {
+      // 1. Stop the form from submitting
+      event.preventDefault();
+      // 2. get the text from that input
+      const listName = this.myInput.current.value;
+      // 3. Change the page to /store/whatever-they-entered
+      this.props.history.push(`/list/${listName}`);
+    };
