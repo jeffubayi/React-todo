8cdbdce0dacac380e4eddbcc1c9b874441400076
jeffubayi <jeffubayi@gmail.com>
2020-09-24 09:41:51 +0300

    List Picker - import maerial ui core components

---
+++ src/components/ListPicker/ListPicker.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+import theme from "../../theme";
+import { getListName } from "../../helpers";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import { Paper, Typography, Button } from "@material-ui/core";
